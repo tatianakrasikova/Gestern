0bf8fbc00d1d598c95e67c8d705a4afa3a84af2a
tatianakrasikova <krasikovatatana535@gmail.com>
2024-09-07 12:47:44 +0200

    task 01

---
+++ src/main/java/task1/Cat.java
@@ -0,0 +1,8 @@
+package task1;
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+    }
+
+}
---
+++ src/main/java/task1/Cat1.java
@@ -0,0 +1,7 @@
+package task1;
+
+public class Cat1 {
+    public static void main(String[] args) {
+        System.out.println("Guten Tag");
+    }
+}
